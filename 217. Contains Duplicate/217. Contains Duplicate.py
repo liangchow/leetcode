73dada61b239efa6fe45020f0392a27372b5041b
DongDong <76621988+liangchow@users.noreply.github.com>
2026-03-08 22:14:09 -0700

    Update solution: 217. Contains Duplicate

--- 217. Contains Duplicate/217. Contains Duplicate.py
+++ 217. Contains Duplicate/217. Contains Duplicate.py
@@ -1,7 +1,7 @@
 #
 # Problem: 217. Contains Duplicate
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/contains-duplicate/submissions/1942498738/
+# Link: https://leetcode.com/problems/contains-duplicate/submissions/1942500925/
 # Language: python3
 # Date: 2026-03-09
 
@@ -10,10 +10,4 @@ from collections import defaultdict
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        map = defaultdict(int)
-        for num in nums:
-            if num not in map:
-                map[num] += 1
-            else:
-                return True
-        return False
+        return (len(nums) > len(set(nums)))
