dca81d1246fb508e97fdfe728a40d516c307a78f
DongDong <76621988+liangchow@users.noreply.github.com>
2026-03-18 15:01:15 -0700

    Update solution: 128. Longest Consecutive Sequence

--- 128. Longest Consecutive Sequence/128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence/128. Longest Consecutive Sequence.py
@@ -1,27 +1,11 @@
 #
 # Problem: 128. Longest Consecutive Sequence
 # Difficulty: Medium
-# Link: https://leetcode.com/problems/longest-consecutive-sequence/submissions/1951814951/
+# Link: https://leetcode.com/problems/longest-consecutive-sequence/solutions/4932112/no-nested-loops-a-true-on-time-algorithm-81ad/
 # Language: python3
 # Date: 2026-03-18
 
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numset = set(nums)
-        longest_streak = 0
-
-        for num in numset:
-            if num-1 not in numset:
-                current = num
-                current_streak = 1
-            
-                while current+1 in numset:
-                    current += 1
-                    current_streak += 1
-
-                longest_streak = max(current_streak, longest_streak)
-
-        return longest_streak
-
-
+        
