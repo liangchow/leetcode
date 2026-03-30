a8d45381a670e24705d3ed4dabbec31ea99ed56f
DongDong <76621988+liangchow@users.noreply.github.com>
2026-03-30 16:56:23 -0700

    Update solution: 20. Valid Parentheses

--- 20. Valid Parentheses/20. Valid Parentheses.py
+++ 20. Valid Parentheses/20. Valid Parentheses.py
@@ -1,7 +1,7 @@
 #
 # Problem: 20. Valid Parentheses
 # Difficulty: Easy
-# Link: https://leetcode.com/problems/valid-parentheses/submissions/1964309796/
+# Link: https://leetcode.com/problems/valid-parentheses/submissions/1964327328/
 # Language: python3
 # Date: 2026-03-30
 
@@ -9,26 +9,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        smap = {"(":")", "[":"]", "{":"}"}
 
         for ch in s:
-            if (ch == "(" or ch == "[" or ch == "{"):
+            if ch in smap:
                 stack.append(ch)
-            elif ch == ")":
-                if (len(stack) == 0 or stack[-1] != "("):
-                    return False
-                else:
-                    stack.pop()
-            elif ch == "]":
-                if (len(stack) == 0 or stack[-1] != "["):
-                    return False
-                else:
-                    stack.pop()
-            elif ch == "}":
-                if (len(stack) == 0 or stack[-1] != "{"):
+            else:
+                if not stack or smap[stack[-1]] != ch:
                     return False
                 else:
                     stack.pop()
         
-        if len(stack) != 0:
-            return False
-        return True
+        return len(stack) == 0
