911df0ac9fcfb6a4afaf17448a2b9407b2a7917b
DongDong <76621988+liangchow@users.noreply.github.com>
2026-04-06 22:46:54 -0700

    Update solution: Unknown Problem

--- Unknown Problem/Unknown Problem.py
+++ Unknown Problem/Unknown Problem.py
@@ -1,22 +1,21 @@
 #
 # Problem: Unknown Problem
-# Difficulty: Easy
-# Link: https://leetcode.com/problems/merge-strings-alternately/submissions/1934100309/?envType=study-plan-v2&envId=leetcode-75
+# Difficulty: Medium
+# Link: https://leetcode.com/problems/non-overlapping-intervals/submissions/1971241004/
 # Language: python3
-# Date: 2026-02-28
+# Date: 2026-04-07
 
 
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-                
-        max_len = max(len(word1), len(word2))
-        res = ""
-
-        for i in range(max_len):
-
-            if i < len(word1):
-                res += word1[i]
-            if i < len(word2): 
-                res += word2[i]
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key=lambda x: x[1])
         
-        return res
+        count = 0
+        last_end = float('-inf')
+
+        for start, end in intervals:
+            if start >= last_end:
+                count += 1
+                last_end = end
+            
+        return len(intervals)-count
